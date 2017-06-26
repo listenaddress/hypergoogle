eab05eabe2297573317f589e27d9b8eab2d3545c
Thomas O'Brien <tbobrien612@gmail.com>
2017-06-26 13:02:58 +0200

    Add listener for mouseup events & display selected text

--- index.js
+++ index.js
@@ -8,24 +8,38 @@ exports.decorateTerm = (Term, { React, notify }) => {
   return class extends React.Component {
     constructor (props, context) {
       super(props, context);
+      this.state = {selectedText: ''};
       this._onTerminal = this._onTerminal.bind(this);
+      this._onMouseUp = this._onMouseUp.bind(this);
     }
 
     _onTerminal (term) {
       if (this.props.onTerminal) this.props.onTerminal(term);
-      const searchBox = document.createElement('div');
-      searchBox.innerHTML = '<h1>Search</h1>';
-      document.body.appendChild(searchBox);
+      this._window = term.document_.defaultView;
+      this._window.addEventListener('mouseup', this._onMouseUp);
+    }
+
+    // Update this.state.selectedText if needed
+    _onMouseUp () {
+      console.log('mouse up');
+      const newText = this._window.getSelection().toString();
+      if (!newText && !this.state.selectedText) return;
+      this.setState({'selectedText': newText});
     }
 
     render () {
-      return React.createElement(Term, Object.assign({}, this.props, {
-        onTerminal: this._onTerminal
-      }));
+      const children = [React.createElement(Term, Object.assign({}, this.props, {
+        onTerminal: this._onTerminal,
+        key: 'term'
+      }))];
+
+      const header = React.createElement('h1', {style: {color: 'red', position: 'absolute', top: 0}}, this.state.selectedText);
+      children.push(header);
+      return React.createElement('div', {style: {width: '100%', height: '100%', position: 'relative'}}, children);
     }
 
     componentWillUnmount () {
-
+      this._window.removeEventListener('mouseup', this._onMouseUp);
     }
   }
 };
