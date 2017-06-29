e231d72df65aa02dd1427ddc5b275e915698d89c
Thomas O'Brien <tbobrien612@gmail.com>
2017-06-29 19:47:01 +0200

    Add text edit and search functionality

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const electron = require('electron');
+
 // The `decorateTerm` hook allows our extension to return a higher order react component
 // It supplies us with:
 // - Term: The terminal component
@@ -8,9 +10,11 @@ exports.decorateTerm = (Term, { React, notify }) => {
   return class extends React.Component {
     constructor (props, context) {
       super(props, context);
-      this.state = {selectedText: ''};
+      this.state = {searchText: ''};
       this._onTerminal = this._onTerminal.bind(this);
       this._onMouseUp = this._onMouseUp.bind(this);
+      this._onChange = this._onChange.bind(this);
+      this._onSearch = this._onSearch.bind(this);
     }
 
     _onTerminal (term) {
@@ -19,22 +23,48 @@ exports.decorateTerm = (Term, { React, notify }) => {
       this._window.addEventListener('mouseup', this._onMouseUp);
     }
 
-    // Update this.state.selectedText if needed
+    // Update this.state.searchText if needed
     _onMouseUp () {
-      console.log('mouse up');
       const newText = this._window.getSelection().toString();
-      if (!newText && !this.state.selectedText) return;
-      this.setState({'selectedText': newText});
+      if (!newText && !this.state.searchText) return;
+      this.setState({'searchText': newText});
+    }
+
+    _onChange (event) {
+      this.setState({'searchText': event.target.value});
+    }
+
+    _onSearch () {
+      const query = this.state.searchText.split(' ').join('+');
+      electron.shell.openExternal('https://www.google.com/#q=' + query);
     }
 
     render () {
-      const children = [React.createElement(Term, Object.assign({}, this.props, {
-        onTerminal: this._onTerminal,
-        key: 'term'
-      }))];
+      let children = [
+        React.createElement(Term, Object.assign({}, this.props, {
+          onTerminal: this._onTerminal,
+          key: 'term'
+        })),
+      ];
 
-      const header = React.createElement('h1', {style: {color: 'red', position: 'absolute', top: 0}}, this.state.selectedText);
-      children.push(header);
+      if (this.state.searchText) {
+        const input = React.createElement(
+          'input',
+          {
+            onChange: this._onChange,
+            value: this.state.searchText
+          }
+        );
+
+        const button = React.createElement(
+          'button',
+          {onClick: this._onSearch},
+          'Search'
+        );
+
+        children.unshift([input, button]);
+      }
+
       return React.createElement('div', {style: {width: '100%', height: '100%', position: 'relative'}}, children);
     }
 
